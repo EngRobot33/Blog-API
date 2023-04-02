f3ea06ddb41971fda85b90e0aff70bfd0fa07eaa
EngRobot33 <hmdkhsrvee@gmail.com>
2023-04-02 22:32:46 +0330

    fix: circular import of cache profile

--- apps/user/services/profiles.py
+++ apps/user/services/profiles.py
@@ -1,11 +1,13 @@
 from django.core.cache import cache
 
 from ..models import User
-from apps.relation.services.relations import count_follower, count_following
-from apps.post.services.posts import count_posts
 
 
 def cache_profile(*, user: User) -> None:
+
+    from apps.relation.services.relations import count_follower, count_following
+    from apps.post.services.posts import count_posts
+
     profile = {
         'posts_count': count_posts(user=user),
         'followers_count': count_follower(user=user),
